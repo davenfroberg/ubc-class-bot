5fa60c2f6c8f68de9fe86b76ee4468177e37e689
Randy Zhu <mail@randyzhu.com>
2023-06-20 10:46:35 -0700

    added embed to make it look cleaner, and added venv to .gitignore

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import discord.ui
 import responses
 from discord import app_commands
 import requests
@@ -22,7 +23,7 @@ def run_discord_bot():
     tree = app_commands.CommandTree(client)
 
     @tree.command(name = "ubcinfo", description = "Get Info on a UBC Course", guild=discord.Object(id=1004543455332147351)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-    async def first_command(interaction, course_code: str, course_number: str):
+    async def first_command(interaction: discord.Interaction, course_code: str, course_number: str):
         code = course_code.upper()
         grade_api = f'https://ubcgrades.com/api/v3/course-statistics/UBCV/{code}/{course_number}'
         request = requests.get(grade_api)
@@ -31,7 +32,12 @@ def run_discord_bot():
             average = round(api_json['average'], 2)
             title = api_json['course_title']
             ssc = f'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept={code}&course={course_number}'
-            await interaction.response.send_message(f'{code} {course_number}: {title} \nAverage: {average}% \n' + ssc)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title=f'{code} {course_number}: {title}',
+                    description=f'Average: {average}% \n[SSC Link]({ssc}) \n[UBCGrades Link](https://ubcgrades.com/course/{code}/{course_number})',
+                )
+            )
         else:
             await interaction.response.send_message(f"{code} {course_number} doesn't seem to exist!")
 
